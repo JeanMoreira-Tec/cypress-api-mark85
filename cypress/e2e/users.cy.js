15d8243432340eb6f8bf809d1bfd61a384da4fcf
Jean Moreira <jean.biomedicina@gmail.com>
2023-06-05 22:48:49 -0300

    Cobertura de testes (validando regras de negocio para criaÃr conta

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,51 +1,55 @@
 
 describe('POST /user', () => {
 
-  it('Register a new user', () => {
+  context('Create an account', () => {
+    it('Register a new user', () => {
 
-    const user = {
-      name: "User Test QA",
-      email: "UserTestQA@quality.com",
-      password: "pwd123"
-    }
-
-    cy.task('deleteUser', user.email)
-
-    cy.postUser(user)
-      .then(response => {
-        expect(response.status).to.eq(200)
-      })
-  })
-
-  it('Dplicate email', () => {
-
-    const user = {
-      name: "User Test QA",
-      email: "UserTestQA@quality.com",
-      password: "pwd123"
-    }
-
-    cy.task('deleteUser', user.email)
-
-    cy.postUser(user)
-
-    cy.postUser(user)
-      .then(response => {
-
-        const { message } = response.body
-
-        expect(response.status).to.eq(409)
-        expect(message).to.eq('Duplicated email!')
-      })
-
+      const user = {
+        name: "User Test QA",
+        email: "UserTestQA@quality.com",
+        password: "pwd123"
+      }
+  
+      cy.task('deleteUser', user.email)
+  
+      cy.postUser(user)
+        .then(response => {
+          expect(response.status).to.eq(200)
+        })
+    })
+  
+    it('Dplicate email', () => {
+  
+      const user = {
+        name: "User Test QA",
+        email: "UserTestQA@quality.com",
+        password: "pwd123"
+      }
+  
+      cy.task('deleteUser', user.email)
+  
+      cy.postUser(user)
+  
+      cy.postUser(user)
+        .then(response => {
+  
+          const { message } = response.body
+  
+          expect(response.status).to.eq(409)
+          expect(message).to.eq('Duplicated email!')
+        })
+  
+    })
   })
 
   context('Required fields', () => {
+
     let user;
+
     beforeEach(() => {
       user = {
         name: "Required Test QA",
-        email: "Requiredfields@quality.com",
+        email: "requiredfields@quality.com",
         password: "pwd123"
       }
     })
@@ -94,5 +98,58 @@ describe('POST /user', () => {
 
   })
 
+  context('Not allowed to be empty', () => {
+    let user;
 
+    beforeEach(() => {
+      user = {
+        name: "Empty Test QA",
+        email: "empty@quality.com",
+        password: "pwd123"
+      }
+    })
+
+    it('Name is not allowed to be empty', () => {
+
+      user.name = ''
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"name\" is not allowed to be empty')
+        })
+    })
+
+    it('Email is not allowed to be empty', () => {
+
+      user.email = ''
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"email\" is not allowed to be empty')
+        })
+    })
+
+    it('Password iis not allowed to be empty', () => {
+
+      user.password = ''
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"password\" is not allowed to be empty')
+        })
+    })
+
+  })
 })
