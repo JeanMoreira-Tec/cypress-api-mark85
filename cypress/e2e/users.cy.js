058a83cb5cb2e0f6a5e220b9226018d0cab2a929
Jean Moreira <jean.biomedicina@gmail.com>
2023-06-05 21:15:42 -0300

    GCricao do gancho para validarcampos obrigatoorios

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -15,16 +15,84 @@ describe('POST /user', () => {
       .then(response => {
         expect(response.status).to.eq(200)
       })
+  })
+
+  it('Dplicate email', () => {
+
+    const user = {
+      name: "User Test QA",
+      email: "UserTestQA@quality.com",
+      password: "pwd123"
+    }
+
+    cy.task('deleteUser', user.email)
+
+    cy.postUser(user)
+
+    cy.postUser(user)
+      .then(response => {
+
+        const { message } = response.body
+
+        expect(response.status).to.eq(409)
+        expect(message).to.eq('Duplicated email!')
+      })
 
   })
-})
+
+  context('Required fields', () => {
+    let user;
+    beforeEach(() => {
+      user = {
+        name: "Required Test QA",
+        email: "Requiredfields@quality.com",
+        password: "pwd123"
+      }
+    })
+
+    it('Name is required', () => {
+
+      delete user.name
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"name\" is required')
+        })
+    })
+
+    it('Email is required', () => {
+
+      delete user.email
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"email\" is required')
+        })
+    })
+
+    it('Password is required', () => {
+
+      delete user.password
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"password\" is required')
+        })
+    })
+
+  })
 
 
-Cypress.Commands.add('postUser', (user) => {
-  cy.api({
-    url: '/users',
-    method: 'POST',
-    body: user,
-    failOnStatusCode: false
-  }).then(response => { return response })
 })
